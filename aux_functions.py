8e92d5a7796d2e7f3b856d2a1bf306f314891c3c
mmomari072 <mmomari072@gmail.com>
2024-05-05 07:47:05 +0300

    few modifications

--- aux_functions.py
+++ aux_functions.py
@@ -6,6 +6,7 @@ Created on Sun Nov 26 20:43:36 2023
 """
 import time
 from copy import deepcopy
+import os,shutil,glob,subprocess
 
 
 class Timer:
@@ -18,7 +19,6 @@ class Timer:
         self.delta = None
         self._deltas_ = [0]
         self._times_ = [0]
-
         pass
     def tic(self):
         self.start = time.time()
@@ -164,9 +164,120 @@ class File:
             f(self,**kw)
         return self
 
+
+class kwargs_handle:
+    def __init__(self,instance,**kwd):
+        pass
+
+
+
+class os_cmd:
+    def rmdir(dirctory:str=None): 
+        try:
+            return shutil.rmtree(dirctory)
+        except:
+            Warning(f"Failed to remove {dirctory}")
+            return 1
+        pass
+    
+    def mkdir(path:str=None):
+        try:
+            os.mkdir(path)
+            return 0 
+        except:
+            Warning(f"Folder {path} is existed")
+            return -123
+        pass
+    
+    def chdir(path:str=None):
+        try:
+            os.chdir(path)
+            return 0
+        except:
+            os.chdir(path)
+            return 1
+    
+    cd=chdir
+    
+    def cpdir(src,dst):
+        _,tail = os.path.split(src)
+        if os.path.isdir(src):
+            shutil.copytree(src, os.path.join(dst, tail))
+        else:
+            pass
+    
+    def cpfile(src,dst):
+        _,tail = os.path.split(src)
+        if os.path.isfile(src):
+            shutil.copy(src, os_cmd.set_fullpath(dst, tail))
+            return 0
+        else:
+            pass
+        pass
+    
+    def cp(src,dst):
+        _,tail = os.path.split(src)
+        if os_cmd.isfile(src):
+            os_cmd.cpfile(src, dst)
+        elif os_cmd.isdir(src):
+            os_cmd.cpdir(src, dst)
+        elif tail.find("*")>=0:
+            flist=glob.glob(src)
+            #print(flist)
+            for f in flist:
+                os_cmd.cp(f,dst)
+            pass
+        else:
+            print("Copying {} object is not supported")
+        pass
+    
+    def isfile(path:str)->bool:
+        return os.path.isfile(path)
+        pass
+    def isdir(path:str)->bool:
+        return os.path.isdir(path)
+        pass
+    
+    def path_type(path:str)->str:#["file","dir","link",None]:
+        for case,fun in {"dir":os_cmd.isdir,"file":os_cmd.isfile}.items():
+            if fun(path):
+                return case
+        return None
+            
+        pass
+    #@property
+    def set_fullpath(dirctory,file,*r)->str:
+        return os.path.join(dirctory, file,*r)
+    
+    def pwd()->str:
+        return os.getcwd()
+    
+    def execute(command:str=None,wkdir:str=None)->int:
+        previous_dirctory=os_cmd.pwd()
+        os_cmd.cd(wkdir)
+        status= os.system(command)
+        os_cmd.cd(previous_dirctory)
+        return status
+    
 if __name__=="__main__":
     A=[]
     for i in progress_bar(name="omari", start=500, end=1000):
         A.append(i)
         time.sleep(.001)
         pass
+    
+    f=File("LICENSE").read()
+    
+    def fun(self):
+        for ky,line in self.search_index.items():
+            for l in line:
+                print(f"{ky}|{self[l]}")
+        return self
+    def fun2(self):
+        print("*"*80)
+        
+    f.add_processing_func(fun,fun2)
+    
+    f.search("is","of")
+    
+    f.process()
---
+++ database.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  2 14:15:15 2024
+
+@author: omari
+"""
+class attribute:
+    def __init__():
+        pass
+
+class database:
+    def __init__(self,name:str=None,description:str=None):
+        self.name:str =name
+        pass
+    
