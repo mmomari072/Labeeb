7303404f90a804a5ec05e6df83ac9c3764bf5788
Mohammad OMARI <mmomari072@gmail.com>
2026-09-17 18:53:47 +0300

    chore: bump version to 2.2.0

--- src/labeeb/__init__.py
+++ src/labeeb/__init__.py
@@ -114,7 +114,7 @@ from .report import write_html_report
 from .logging_config import CaseLoggerAdapter, configure_logging
 from .utils.file_io import File, evaluate_expression, format_value
 
-__version__ = "2.1.0"
+__version__ = "2.2.0"
 __author__ = "Mohammed Omari"
 
 __all__ = [
